bcc3642f8c02d527e56d81ab68e4753cae66b7a1
unknown <davidchinski@gmail.com>
2024-08-22 20:05:14 -0300

    hacer ObtenerPreguntas()y ObtenerDificultades() de la BD.CS

--- Models/BD.cs
+++ Models/BD.cs
@@ -6,20 +6,56 @@ public static class BD
 {
     const string CONNECTION_STRING = @"Server=localhost;Database=PreguntadOrt;Trusted_Connection=True;";
 
-    private static List<Categoria> _ListadoCategorias = new List<Categoria>();
+    
     public static List<Categoria> ObtenerCategorias()
-    {
-       using (SqlConnection BD = new SqlConnection(CONNECTION_STRING))
-        {
+    {   
+        using (SqlConnection BD = new SqlConnection(CONNECTION_STRING))
+        {   
             string sql = "SELECT * FROM Categorias";
-            _ListadoCategorias = BD.Query<Categoria>(sql).ToList();
-            
+            return BD.Query<Categoria>(sql).ToList();
+        }
+    }
+
+
+    
+    public static List<Dificultad> ObtenerDificultades()
+    {
+        using (SqlConnection BD = new SqlConnection(CONNECTION_STRING))
+        {
+            string sql = "SELECT * FROM Dificultades";
+            return BD.Query<Dificultad>(sql).ToList();
+        }
+    }
+
+
+    public static List<Pregunta> _ListadoPreguntas = new List<Pregunta>();
+    public static List<Pregunta> ObtenerPreguntas(int dificultad, int categoria)
+    {
+        using(SqlConnection BD = new SqlConnection(CONNECTION_STRING))
+        {
+            string sql = "SELECT * FROM Preguntas WHERE (@dificultad = -1 OR IdDificultad = @dificultad) AND (@categoria = -1 OR IdCategoria = @categoria";
+            _ListadoPreguntas = BD.Query<Pregunta>(sql, new {dificultad, categoria} ).ToList();
         }
 
-        return _ListadoCategorias;
+        return _ListadoPreguntas;
+
+    }
+
+    public static List<Respuesta> _ListadoRespuestas = new List<Respuesta>();
+
+    public static List<Respuesta> ObtenerRespuestas(List<Pregunta> preguntas)
+    {
+        using(SqlConnection BD = new SqlConnection(CONNECTION_STRING))
+        {
+            
+
+        }
 
 
-    }   
+    }
+
+
+    
 
 
 
