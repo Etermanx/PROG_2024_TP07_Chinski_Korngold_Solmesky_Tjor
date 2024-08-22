5c66d077b134799486d33269774f9180b4b83296
Lucas Tjor <tjor75@gmail.com>
2024-08-21 22:51:28 -0300

    Crear proyecto MVC + BD.cs + sacar JQuery y archivos innecesarios

---
+++ Models/BD.cs
@@ -0,0 +1,7 @@
+using System.Data.SqlClient;
+using Dapper;
+
+public static class BD
+{
+    const string CONNECTION_STRING = @"Server=localhost;Database=PreguntadOrt;Trusted_Connection=True;";
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PROG_2024_TP07_Chinski_Korngold_Tjor.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
