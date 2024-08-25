c30bced7bc36ee69162e5ef9cda0909bb4db751d
Lucas Tjor <tjor75@gmail.com>
2024-08-25 02:11:48 -0300

    Modificar cantidad de varchars y crear ejemplos SQL

---
+++ sql/CrearCategorias.sql
@@ -0,0 +1,11 @@
+USE PreguntadOrt
+
+INSERT INTO Categorias(Nombre, Foto)
+VALUES
+    ('Geografia', 'https://cdn.pixabay.com/photo/2017/02/01/10/00/cartography-2029310_960_720.png'),
+    ('Ciencia', 'https://cdn.pixabay.com/photo/2013/07/13/01/07/chemistry-155121_960_720.png'),
+    ('Historia', 'https://cdn.pixabay.com/photo/2023/03/12/11/31/colliseum-7846539_960_720.png'),
+    ('Deportes', 'https://cdn.pixabay.com/photo/2013/07/13/10/50/tennis-ball-157884_960_720.png'),
+    ('Arte y literatura', 'https://cdn.pixabay.com/photo/2016/07/12/19/20/palette-1512871_960_720.png'),
+    ('Entretenimiento', 'https://cdn.pixabay.com/photo/2013/07/12/14/47/tv-148809_960_720.png'),
+    ('Cambalache', NULL);
