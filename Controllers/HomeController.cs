052447586c74ef2c685b9651c3a36bdccd97e81f
Lucas Tjor <tjor75@gmail.com>
2024-08-26 10:31:49 -0300

    Agrgear Contendido al Respuesta.cshtml

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -82,5 +82,10 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+        public IActionResult Respuesta()
+    {
+        return View();
+    }
     
 }
